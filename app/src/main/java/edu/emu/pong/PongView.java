d68b866252877c2051aa921967c9d4046e46f246
Joel Christophel <joelamoschristophel@gmail.com>
2016-11-29 17:15:26 -0500

    Integrate paddle

--- app/src/main/java/edu/emu/pong/PongView.java
+++ app/src/main/java/edu/emu/pong/PongView.java
@@ -6,6 +6,7 @@ import android.graphics.Paint;
 import android.os.Handler;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
 
@@ -38,6 +39,9 @@ public class PongView extends View {
     private long lastLoopTime;
     private int displayWidth;
     private int displayHeight;
+    private Paddle paddle;
+    private Ball ball;
+    private boolean started = false;
 
     public PongView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -57,42 +61,102 @@ public class PongView extends View {
         frameHandler.postDelayed(frameRefresher, 1000 / FRAMES_PER_SECOND);
     }
 
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        boolean leftSide = event.getX() < displayWidth / 2;
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_MOVE:
+                if (!started) {
+                    started = true;
+                    ball.setDownwardVelocityScale(1);
+                    ball.setRightwardVelocityScale(1);
+                }
+                if (leftSide) {
+                    paddle.setRightMovement(false);
+                    paddle.setLeftMovement(true);
+                } else {
+                    paddle.setLeftMovement(false);
+                    paddle.setRightMovement(true);
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                if (leftSide) {
+                    paddle.setLeftMovement(false);
+                } else {
+                    paddle.setRightMovement(false);
+                }
+                break;
+        }
+        return true;
+    }
+
     private void createEntities() {
-        float ballWidth = (displayWidth > displayHeight ? displayWidth : displayHeight) / 20.5f;
-        Ball ball = new Ball(0, 0, ballWidth, ballWidth, 1, 1);
+        //float longestDimension = displayWidth > displayHeight ? displayWidth : displayHeight;
+        float paddleWidth = displayWidth / 4;
+        float paddleHeight = displayWidth / 60;
+        float paddleX = (displayWidth / 2) - (paddleWidth / 2);
+        float paddleY = displayHeight - paddleHeight;
+        paddle = new Paddle(paddleX, paddleY, paddleWidth, paddleHeight);
+        float ballWidth = displayWidth / 20.5f;
+        ball = new Ball(0, 0, ballWidth, ballWidth, 0, 0);
     }
 
     private void updateEntities(float delta) {
+        paddle.setX(adjustPosition(paddle, paddle.getX() + (delta * paddle.getRightwardVelocity()), true));
         for (Entity entity: Entity.getInstances()) {
-            if (onXWall(entity)) {
-                entity.setRightwardVelocityScale(-1 * entity.getRightwardVelocityScale());
-            } else if (onYWall(entity)) {
-                entity.setDownwardVelocityScale(-1 * entity.getDownwardVelocityScale());
+            if (!entity.equals(paddle)) {
+                entity.setX(adjustPosition(entity, entity.getX() + (delta * entity.getRightwardVelocity()), true));
+                entity.setY(adjustPosition(entity, entity.getY() + (delta * entity.getDownwardVelocity()), false));
+                if (entity instanceof Ball) {
+                    if (onWall(entity)) {
+                        entity.setRightwardVelocityScale(-1 * entity.getRightwardVelocityScale());
+                    } else if (onCeiling(entity) || onPaddle(entity) || onFloor(entity)) {
+                        entity.setDownwardVelocityScale(-1 * entity.getDownwardVelocityScale());
+                    }
+                }
             }
-            entity.setX(adjustPosition(entity, entity.getX() + (delta * entity.getRightwardVelocity()), true));
-            entity.setY(adjustPosition(entity, entity.getY() + (delta * entity.getDownwardVelocity()), false));
         }
     }
 
     private float adjustPosition(Entity entity, float targetPosition, boolean xCoordinate) {
         float position = xCoordinate ? entity.getX() : entity.getY();
         float minimumBound = 0;
-        float maximumBound = xCoordinate ? displayWidth : displayHeight;
+        float maximumBound = xCoordinate ? displayWidth : displayHeight + entity.getHeight() + 100;
         float adjustedPosition = targetPosition;
         float offset = (xCoordinate ? entity.getWidth() : entity.getHeight());
         if (targetPosition < minimumBound) {
             adjustedPosition = minimumBound;
+            System.out.println("Under min bound");
         } else if (targetPosition + offset > maximumBound) {
+            //System.out.println("Exceeded max bound");
             adjustedPosition = maximumBound - offset;
+        }
+        if (entity instanceof Ball && !xCoordinate && inPaddleDomain(entity) &&
+                entity.getY() > displayHeight - entity.getHeight() - paddle.getHeight() &&
+                entity.getY() < displayHeight - entity.getHeight()) {
+            System.out.println("Exceeded paddle bound");
+            adjustedPosition = displayHeight - entity.getHeight() - paddle.getHeight();
         }
         return adjustedPosition;
     }
 
-    public boolean onXWall(Entity entity) {
+    public boolean onWall(Entity entity) {
         return entity.getX() == 0 || entity.getX() == displayWidth - entity.getWidth();
     }
 
-    public boolean onYWall(Entity entity) {
-        return entity.getY() == 0 || entity.getY() == displayHeight - entity.getHeight();
+    public boolean onCeiling(Entity entity) {
+        return entity.getY() <= 0;
+    }
+
+    public boolean onFloor(Entity entity) {
+        return entity.getY() == displayHeight + 100;
+    }
+
+    public boolean inPaddleDomain(Entity entity) {
+        return paddle.getX() - entity.getWidth() < entity.getX() && entity.getX() < paddle.getX() + paddle.getWidth();
+    }
+
+    public boolean onPaddle(Entity entity) {
+        return inPaddleDomain(entity) && entity.getY() == displayHeight - entity.getHeight() - paddle.getHeight();
     }
 }
